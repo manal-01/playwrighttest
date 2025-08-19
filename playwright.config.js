32f9755eb54b5cb3d8731907584ae567b9e61723
manal-01 <83735247+manal-01@users.noreply.github.com>
2025-08-19 15:44:05 +0200

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   /*reporter: 'html',*/
-  reporter: [['junit', { outputFile: 'results/results.xml' }]],
+  //reporter: [['junit', { outputFile: 'results/results.xml' }]],
+  reporter: 'junit',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
